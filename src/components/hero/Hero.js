219fd340ec99d1dda378efb9dd89d589b9fbfad6
Ubed2003 <147146015+Ubed2003@users.noreply.github.com>
2024-03-28 22:47:40 +0530

    asf

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,9 +8,9 @@ import './style.css'
 const Hero = () => {
   const [currenSlide, setCurrentSlide] = useState(0)
   const data = [
-    "https://images.pexels.com/photos/5746087/pexels-photo-5746087.jpeg?auto=compress&cs=tinysrgb&w=1660&dpr=1",
-    "https://images.pexels.com/photos/274973/pexels-photo-274973.jpeg?auto=compress&cs=tinysrgb&w=1600&dpr=1",
-    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1660&dpr=1",
+    "https://images.pexels.com/photos/5746087/pexels-photo-5746087.jpeg?auto=compress&cs=tinysrgb&w=1366",
+    "https://images.pexels.com/photos/274973/pexels-photo-274973.jpeg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1200",
   ];
   const prevSlide = () =>{
     setCurrentSlide(currenSlide === 0 ? 2 : (prev) => prev - 1)
@@ -23,11 +23,11 @@ const Hero = () => {
 
 
     <section className= ''>
-      <div className='relative ' >
-      <div className=" slider flex w-screen m-0 p-0  bg-cover"style={{transform:`translateX(-${currenSlide * 100}vw) `}}>
-        <img className=" " src={data[0]} alt="" />
-        <img className=" " src={data[1 ]} alt="" />
-        <img className=" " src={data[2]} alt="" />
+      <div className='relative slider' >
+      <div className="  flex m-0 p-0 bg-cover m-0 p-0 "style={{transform:`translateX(-${currenSlide * 100}vw) `}}>
+        <img className="w-screen " src={data[0]} alt="" />
+        <img className="" src={data[1 ]} alt="" />
+        <img className="" src={data[2]} alt="" />
        
         </div>
         <div className="icons absolute w-[100px] m-auto right-0 left-0 flex bottom-[10px] gap-3  ">
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -24,13 +24,13 @@ const ProductDetails = () => {
     );
   }
   // destructure product
-  const { title, price, description, image, category } = product;
+  const { title, price, description, images, category } = product;
   return (
     <section className="pt-32 pb-12 lg:py-32 h-screen">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row  items-center">
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0 ">
-            <img className="max-w[200px] lg:max-w-sm" src={image} />
+            <img className="max-w[200px] lg:max-w-sm" src={images} />
           </div>
           <div className="flex-1 text-center lg:text-left ">
             <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto ">
