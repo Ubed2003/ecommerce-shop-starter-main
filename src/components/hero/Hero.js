ce7aa84d5f78cad8c64671929c606e31e375204e
Ubed2003 <147146015+Ubed2003@users.noreply.github.com>
2024-03-31 15:15:27 +0530

    sag

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,9 +8,9 @@ import './style.css'
 const Hero = () => {
   const [currenSlide, setCurrentSlide] = useState(0)
   const data = [
-    "https://images.pexels.com/photos/5746087/pexels-photo-5746087.jpeg?auto=compress&cs=tinysrgb&w=1366",
-    "https://images.pexels.com/photos/274973/pexels-photo-274973.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1200",
+    "https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg",
+    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750dpr=1",
   ];
   const prevSlide = () =>{
     setCurrentSlide(currenSlide === 0 ? 2 : (prev) => prev - 1)
@@ -23,11 +23,11 @@ const Hero = () => {
 
 
     <section className= ''>
-      <div className='relative slider' >
-      <div className="  flex m-0 p-0 bg-cover m-0 p-0 "style={{transform:`translateX(-${currenSlide * 100}vw) `}}>
-        <img className="w-screen " src={data[0]} alt="" />
-        <img className="" src={data[1 ]} alt="" />
-        <img className="" src={data[2]} alt="" />
+      <div className='relative section h-screen w-screen ' >
+      <div className=" slider flex m-0 p-0 h-full m-0 p-0  duration-500 transition-all  "style={{transform:`translateX(-${currenSlide * 100}vw) `}}>
+        <img className="w-screen h-full object-cover" src={data[0]} alt="" />
+        <img className="w-screen  h-full object-cover " src={data[1 ]} alt="" />
+        <img className="w-screen   h-full object-cover " src={data[2]} alt="" />
        
         </div>
         <div className="icons absolute w-[100px] m-auto right-0 left-0 flex bottom-[10px] gap-3  ">
