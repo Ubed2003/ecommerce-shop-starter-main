55e7f4378d25bdf62c35d4cdb5c2da1495f88191
Ubed2003 <147146015+Ubed2003@users.noreply.github.com>
2024-04-04 09:43:09 +0530

    gdsa

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,9 +8,10 @@ import './style.css'
 const Hero = () => {
   const [currenSlide, setCurrentSlide] = useState(0)
   const data = [
-    "https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrg&dpr=1",
-    "https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg",
-    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrg&dpr=1",
+    "https://images.pexels.com/photos/3755706/pexels-photo-3755706.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/19090/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/51383/photo-camera-subject-photographer-51383.jpeg"
+,
   ];
   const prevSlide = () =>{
     setCurrentSlide(currenSlide === 0 ? 2 : (prev) => prev - 1)
@@ -23,11 +24,11 @@ const Hero = () => {
 
 
     <section className= ''>
-      <div className='relative section h-screen w-screen ' >
-      <div className=" slider flex m-0 p-0  h-screen m-0 p-0  duration-500 transition-all w-screen  "style={{transform:`translateX(-${currenSlide * 100}vw) `}}>
-        <img className="w-screen h-screen  object-cover " src={data[0]} alt="" />
-        <img className="w-screen  h-screen  object-cover " src={data[1 ]} alt="" />
-        <img className="w-screen   h-screen   object-cover " src={data[2]} alt="" />
+      <div className='relative    w-screen ' >
+      <div className=" slider flex m-0 p-0  m-0 p-0  duration-500 transition-all w-screen  "style={{transform:`translateX(-${currenSlide * 100}vw) `}}>
+        <img className="w-screen  object-cover " src={data[0]} alt="" />
+        <img className="w-screen object-cover " src={data[1 ]} alt="" />
+        <img className="w-screen    object-cover " src={data[2]} alt="" />
        
         </div>
         <div className="icons absolute w-[100px] m-auto right-0 left-0 flex bottom-[10px] gap-3  ">
